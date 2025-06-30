8edbcc7bbf8b43b08d8d96b1508d7de039865a48
Joval-pixel <joaopaulodetoledo@gmail.com>
2025-06-30 19:00:54 -0300

    Atualizar o bolhas.js

--- bolhas.js
+++ bolhas.js
@@ -1,9 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const container = document.getElementById("bubbles-container");
-
   const bubble = document.createElement("div");
-  bubble.classList.add("bubble"); // <- ESSENCIAL
+  bubble.className = "bubble";
   bubble.textContent = "💬 B3 bolha simulada!";
-
   container.appendChild(bubble);
 });
