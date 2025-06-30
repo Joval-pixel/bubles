0bdd870164df495ccc657c1c6603f7018685d013
Joval-pixel <joaopaulodetoledo@gmail.com>
2025-06-30 17:09:50 -0300

    Add files via upload

---
+++ bolhas.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const container = document.getElementById("bubbles-container");
+  const bubble = document.createElement("div");
+  bubble.textContent = "💬 B3 bolha simulada!";
+  bubble.style.padding = "20px";
+  bubble.style.backgroundColor = "#cceeff";
+  bubble.style.borderRadius = "50%";
+  bubble.style.display = "inline-block";
+  container.appendChild(bubble);
+});
