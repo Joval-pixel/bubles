f435cfc3fa545975e482bc1aab2b50054a51e7b5
Joval-pixel <joaopaulodetoledo@gmail.com>
2025-06-30 18:05:38 -0300

    Update and rename Bolhas.js to bolhas.js

--- bolhas.js
+++ bolhas.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const container = document.getElementById("bubbles-container");
 
   const bubble = document.createElement("div");
+  bubble.classList.add("bubble"); // <- ESSENCIAL
   bubble.textContent = "💬 B3 bolha simulada!";
-  bubble.classList.add("bubble");
 
   container.appendChild(bubble);
 });
