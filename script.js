78add670a955411b7b9035b2f6a8eab5d4392814
Joval-pixel <joaopaulodetoledo@gmail.com>
2025-08-12 10:42:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ const TOP_N = IS_MOBILE ? 30 : 100;
 /* Física (mobile mais lento) */
 const HEADER_SAFE      = 84;
 const WALL_MARGIN      = IS_MOBILE ? 18 : 10;
-const FRICTION         = IS_MOBILE ? 0.998 : 0.985;
+const FRICTION         = IS_MOBILE ? 0.698 : 0.685;
 const MAX_SPEED        = IS_MOBILE ? 0.12  : 0.90;
-const START_VEL        = IS_MOBILE ? 0.05  : 0.45;
+const START_VEL        = IS_MOBILE ? 0.05  : 0.15;
 const REPULSE_COLLIDE  = IS_MOBILE ? 0.40  : 0.20; // choque imediato
 const BORDER_WIDTH     = 2.5;
 const COLLISION_PASSES = IS_MOBILE ? 5 : 1;
