6102f22ce3de572f78d9ebf9401ab7c87184b008
Joval-pixel <joaopaulodetoledo@gmail.com>
2026-02-04 08:45:50 -0300

    Revise layout metadata for improved SEO
    Updated metadata title and description for better clarity.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,19 @@
-import './globals.css'
+import "./globals.css";
 
 export const metadata = {
-  title: 'Bubles IA',
-  description: 'Plataforma de IA e ChatGPT'
-}
+  title: "Bubles IA | ChatGPT e Inteligência Artificial",
+  description:
+    "Aprenda a usar ChatGPT e Inteligência Artificial para ganhar tempo, produtividade e dinheiro.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="pt-BR">
       <body>{children}</body>
     </html>
-  )
+  );
 }
