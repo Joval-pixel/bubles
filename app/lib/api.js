13c2498518f9a4d5053316ed7de18561b0e61de2
Joval-pixel <joaopaulodetoledo@gmail.com>
2026-04-22 13:40:33 -0300

    Update api.js

--- app/lib/api.js
+++ app/lib/api.js
@@ -2,7 +2,7 @@ export async function getJogos() {
   const res = await fetch("https://api-football-v1.p.rapidapi.com/v3/fixtures?live=all", {
     method: "GET",
     headers: {
-      "X-RapidAPI-Key": "80f66ec27fd8c6e19f32bf661be443697e561810556491ebc6c2897109448d67",
+      "X-RapidAPI-Key": process.env.API_KEY,
       "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
     },
     cache: "no-store"
