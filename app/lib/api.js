026f296ad18ca21288ca7da88378e87a1ccfc222
Joval-pixel <joaopaulodetoledo@gmail.com>
2026-04-22 12:38:31 -0300

    Update api.js

--- app/lib/api.js
+++ app/lib/api.js
@@ -1,14 +1,19 @@
 export async function getJogos() {
-  const res = await fetch(
-    "https://v3.football.api-sports.io/fixtures?date=2026-04-22&league=71&season=2024",
-    {
-      headers: {
-        "x-apisports-key": process.env.API_KEY,
-      },
-    }
-  );
+  try {
+    const res = await fetch(
+      "https://v3.football.api-sports.io/fixtures?date=2026-04-22&league=71&season=2024",
+      {
+        headers: {
+          "x-apisports-key": process.env.API_KEY,
+        },
+        cache: "no-store"
+      }
+    );
 
-  const data = await res.json();
+    const data = await res.json();
 
-  return data.response || [];
+    return data.response || [];
+  } catch (e) {
+    return [];
+  }
 }
