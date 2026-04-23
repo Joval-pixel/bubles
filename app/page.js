458bbe7b25f54deb2889712f9e7ac30bcc0b0488
Joval-pixel <joaopaulodetoledo@gmail.com>
2026-04-23 08:36:59 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,16 @@
-export default function Home() {
+import { getJogos } from "./lib/api";
+
+export default async function Home() {
+  const jogos = await getJogos();
+
   return (
     <div>
-      <h1>Rodando 🚀</h1>
+      <h1>Jogos ao vivo</h1>
+      {jogos?.map((jogo) => (
+        <div key={jogo.fixture.id}>
+          {jogo.teams.home.name} vs {jogo.teams.away.name}
+        </div>
+      ))}
     </div>
   );
 }
