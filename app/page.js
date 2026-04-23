6db71b61c5c95d18ade4709472d9917df5f78885
Joval-pixel <joaopaulodetoledo@gmail.com>
2026-04-23 09:10:16 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,3 +1,32 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  return <h1>FUNCIONANDO</h1>;
+  const [jogos, setJogos] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/refresh")
+      .then((res) => res.json())
+      .then((data) => {
+        setJogos(data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Jogos ao vivo</h1>
+
+      {jogos.length === 0 ? (
+        <p>Carregando...</p>
+      ) : (
+        jogos.map((jogo) => (
+          <div key={jogo.fixture.id} style={{ marginBottom: 10 }}>
+            ⚽ {jogo.teams.home.name} vs {jogo.teams.away.name}
+          </div>
+        ))
+      )}
+    </div>
+  );
 }
