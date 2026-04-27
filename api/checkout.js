9e2c2bda81d642ce8ce47bb014be4a7854fe4ff4
Joval-pixel <joaopaulodetoledo@gmail.com>
2026-04-27 10:04:58 -0300

    Create checkout.js

---
+++ api/checkout.js
@@ -0,0 +1,27 @@
+import Stripe from "stripe";
+
+export default async function handler(req, res) {
+  try {
+    const stripe = new Stripe(process.env.STRIPE_SECRET);
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "subscription",
+      line_items: [
+        {
+          price: process.env.STRIPE_PRICE_ID,
+          quantity: 1,
+        },
+      ],
+      success_url: "https://bubles.com.br/success",
+      cancel_url: "https://bubles.com.br/cancel",
+    });
+
+    return res.status(200).json({ url: session.url });
+
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+}
