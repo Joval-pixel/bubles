178cff977193d8985ed255670edbe7a9e36a3280
Joval-pixel <joaopaulodetoledo@gmail.com>
2026-04-30 11:31:39 -0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./styles.css";
+
+class RootBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error) {
+    console.error(error);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div className="app-shell">
+          <div className="empty-state">
+            <h1>Bubles Copa 2026</h1>
+            <p>O radar continua estavel mesmo se alguma renderizacao falhar.</p>
+          </div>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RootBoundary>
+      <App />
+    </RootBoundary>
+  </React.StrictMode>
+);
