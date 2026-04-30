87ec7cc469df45d3cc53c6637906a5dcf9484c05
Joval-pixel <joaopaulodetoledo@gmail.com>
2026-04-30 10:04:08 -0300

    Delete api directory

--- api/login.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  const { user, pass } = req.body;
-
-  if (user === "admin" && pass === "123") {
-    return res.status(200).json({ ok: true });
-  }
-
-  return res.status(401).json({ error: "Login inválido" });
-}
