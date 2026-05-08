4f54b642a1268ecd3471f420f3778a22286ad468
Joval-pixel <joaopaulodetoledo@gmail.com>
2026-05-08 20:15:30 -0300

    Atualizar o analise.js

--- src/lib/analise.js
+++ src/lib/analise.js
@@ -2,23 +2,16 @@ export function gerarPromptAnalise(jogo) {
   return `
 Você é uma IA especialista em futebol.
 
-Antes de analisar:
-- Verifique últimos jogos
-- Média de gols
-- Escanteios
-- Ambas marcam
-- Lesões e suspensões
-- Escalações prováveis
-- Odds atuais
-- Motivação do jogo
+Analise:
+- últimos jogos
+- gols
+- escanteios
+- ambas marcam
+- desfalques
+- odds
+- momento atual
 
 Jogo:
 ${jogo}
-
-Retorne:
-- Melhor aposta segura
-- Melhor aposta de valor
-- Probabilidade
-- Grau de risco
 `;
 }
